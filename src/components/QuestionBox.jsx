14d4421ee067ce7a83f4c69fe5b21f08c0c5d886
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-01-19 16:49:35 +0530

    done

--- src/components/QuestionBox.jsx
+++ src/components/QuestionBox.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './QuestionBox.css'
+import questions from '../questions'
 
 export default function QuestionBox() {
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+
+  const nextQuestion = () => {
+    if(currentQuestion < questions.length - 1){
+      setCurrentQuestion(currentQuestion+1)
+    }
+  }
+
+  const [highlight, setHighlight] = useState("black");
+
   return (
     <div className='questionBox'>
       <div className='container'>
 
         <div className='leftContainer'></div>
         <div className='rightContainer'>
-          <button className='theme'>Dark</button>
-          <h3>Question 1 out of 5</h3>
-          <h1>What is react js?</h1>
+          <button className='theme'>
+            Dark
+            <img src="../assets/moon.png"/>
+          </button>
+          <h3>Question <span className='currQuestion'>{currentQuestion+1}</span> out of <span className='NumOfQuestions'>{questions.length}</span></h3>
+          <h1 style={{color: highlight}} >{questions[currentQuestion].text}</h1>
 
           <div className='options'>
-            <div className='option'>Server-side framework</div>
-            <div className='option'>user interface framework</div>
-            <div className='option'>both a and b</div>
-            <div className='option'>none of the above</div>
+            {questions[currentQuestion].options.map((option, index) => {
+              return(
+                <button className='option' key={index} onClick={nextQuestion}>
+                  {option.text}
+                </button>
+              )
+            })}
           </div>
 
           <div className='buttonsContainer'>
-            <button className='highlight'>Highlight</button>
-            <button className='removeHighlight'>Remove Highlight</button>
+            <button className='highlight' onClick={() => setHighlight("rgba(111, 83, 221, 1)")}>Highlight</button>
+            <button className='removeHighlight'onClick={() => setHighlight("black")}>Remove Highlight</button>
           </div>
 
         </div>
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
+import "../components/Result.css"
 
 export default function Result() {
   return (
     <div className='result'>
       {/* result container */}
-      <div className='container'>
+      <div className='resultContainer'>
 
-        <div className='leftContainer'>
-        <img className='logo' src="../assets/leftSide img.png" alt='logo'/>
-        </div>
-
-        <div className='rightContainer'>
-          <button className='theme'>Dark</button>
-          <h1>Final Result</h1>
-          <h3>1 out of 5 correct - (20%)</h3>
+        <div className='resultLeftContainer'></div>
+        <div className='resultRightContainer'>
+          <button className='resultTheme'>Dark</button>
+          <h1 className='finalResult'>Final Result</h1>
+          <h3 className='score'>1 out of 5 correct - (20%)</h3>
 
           <button className='restart'>Restart Game</button>
         </div>
